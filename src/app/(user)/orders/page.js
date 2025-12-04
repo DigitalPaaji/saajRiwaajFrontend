7151d5bddc736ccb1d2bc0fd1ca4fed1f8a5a06f
jonty1231 <jontythakur68@gmail.com>
2025-12-04 10:44:18 +0530

    done

--- src/app/(user)/orders/page.js
+++ src/app/(user)/orders/page.js
@@ -17,6 +17,8 @@ const [cancelReason, setCancelReason] = useState("");
 const [customReason, setCustomReason] = useState("");
 
 
+console.log(orders,"assss")
+
   const fetchOrders = useCallback(async () => {
     try {
       const res = await fetch(
@@ -191,202 +193,191 @@ const cancelReasonOptions = [
 
 
   return (
-    <div>
-    
-      <Banner title="My Orders" />
+  <div>
+    <Banner title="My Orders" />
 
-    
-      <div className="px-4 sm:px-8 lg:px-24 xl:px-60 mx-auto my-16">
-        {loading ? (
-            <Skeleton count={5} height={40} className="mb-2 rounded" />
-        ) : orders.length === 0 ? (
-          <div className="flex flex-col items-center justify-center py-20">
-            <h3 className="text-2xl font-semibold mb-4 text-center text-[#B67032]">
-              No Orders Found
-            </h3>
-            <p className="text-gray-700 text-lg text-center">
-              Please login to view your orders or place a new one.
-            </p>
-         <button
-  onClick={() => setIsAuthOpen(true)}
- className="w-fit px-4 bg-[#B67032] text-white py-2 rounded mt-6"
+    <div className="px-4 sm:px-8 lg:px-24 xl:px-60 mx-auto my-16">
+      {loading ? (
+        <Skeleton count={5} height={40} className="mb-2 rounded" />
+      ) : orders.length === 0 ? (
+        <div className="flex flex-col items-center justify-center py-20">
+          <h3 className="text-2xl font-semibold mb-4 text-center text-[#B67032]">
+            No Orders Found
+          </h3>
+          <p className="text-gray-700 text-lg text-center">
+            Please login to view your orders or place a new one.
+          </p>
 
->
-  Login / Signup
+          <button
+            onClick={() => setIsAuthOpen(true)}
+            className="w-fit px-4 bg-[#B67032] text-white py-2 rounded mt-6"
+          >
+            Login / Signup
+          </button>
+        </div>
+      ) : (
+        <div className="space-y-6">
+          {orders.map((order) => {
+            return (
+              <div
+                key={order._id}
+                className="bg-white rounded-xl shadow-md border border-gray-100 hover:shadow-lg transition p-5"
+              >
+                <div className="flex justify-between mb-3">
+                  <h3 className="font-semibold text-lg text-gray-800">
+                    Order ID: {order._id}
+                  </h3>
 
-</button>
-          </div>
-        ) : (
-          <div className="space-y-6">
-            {orders.map((order) =>
-              order.items.map((item) => {
-                const product =
-                  allProducts.find((p) => p._id === item.product) || {};
-
-                return (
-                  <div
-                    key={item._id}
-                    className="bg-white rounded-xl shadow-md border border-gray-100 hover:shadow-lg transition p-5"
+                  <span
+                    className={`text-xs sm:text-sm font-medium px-3 py-1 rounded-lg ${
+                      order.orderStatus === "delivered"
+                        ? "bg-green-100 text-green-700"
+                        : order.orderStatus === "confirmed"
+                        ? "bg-amber-100 text-amber-600"
+                        : order.orderStatus === "cancelled"
+                        ? "bg-red-100 text-red-600"
+                        : order.orderStatus === "shipped"
+                        ? "bg-blue-100 text-blue-600"
+                        : "bg-yellow-100 text-yellow-600"
+                    }`}
                   >
-                   
-                    <div className="flex flex-wrap  md:grid  md:grid-cols-5 lg:grid-cols-12 px-4 place-items-center">
-                      <div className="lg:col-span-1 w-20 h-20 flex-shrink-0 rounded-lg overflow-hidden bg-gray-50">
-                        <Link href={`/product/${product.name}/${product._id}`}>
-                          <Image
-                            src={product.images?.[0] || "/Images/img.webp"}
-                            alt={product.name || " "}
-                            width={400}
-                            height={400}
-                            className="w-full h-full object-cover"
-                          />
-                        </Link>
-                      </div>
-                      <div className="lg:col-span-1 xl:col-span-5 flex flex-col w-full xl:ml-6">
-                        <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-6">
-                          <p className="text-lg font-medium text-gray-700 font-serif">
-                            {product.name || ""}
+                    {order.orderStatus.charAt(0).toUpperCase() +
+                      order.orderStatus.slice(1)}
+                  </span>
+                </div>
+
+                <div className="space-y-4 border-t pt-4">
+                  {order.items.map((item) => {
+                    const product =
+                      allProducts.find((p) => p._id === item.product) || {};
+
+                    return (
+                      <div
+                        key={item._id}
+                        className="flex gap-4 items-center border-b pb-3"
+                      >
+                        <div className="w-20 h-20 rounded-lg bg-gray-50 overflow-hidden">
+                          <Link
+                            href={`/product/${product?.name}/${product?._id}`}
+                          >
+                            <Image
+                              src={product.images?.[0] || "/Images/img.webp"}
+                              width={400}
+                              height={400}
+                              alt="Product"
+                              className="w-full h-full object-cover"
+                            />
+                          </Link>
+                        </div>
+
+                        <div className="flex-1">
+                          <p className="text-lg font-medium text-gray-700">
+                            {product.name}
+                          </p>
+                          <p className="text-sm text-gray-500">
+                            Qty: {item.quantity}
                           </p>
                         </div>
-                        <p className="text-sm text-gray-500 mt-1">
-                          {order.orderStatus === "delivered"
-                            ? `Delivered on ${new Date(
-                                order.updatedAt
-                              ).toLocaleDateString()}`
-                            : order.orderStatus === "cancelled"
-                            ? "Cancelled as per your request"
-                            : `Estimated by ${new Date(
-                                order.createdAt
-                              ).toLocaleDateString()}`}
-                        </p>
+
+                        <div className="text-lg font-semibold text-[#99571d]">
+                          ₹{item.price}
+                        </div>
                       </div>
-                      <div className="lg:col-span-1 flex md:flex-col items-center md:items-start gap-6 md:gap-0">
-                        <p className="text-sm text-gray-700">
-                          Qty:{" "}
-                          <span className="font-medium">{item?.quantity}</span>
-                        </p>
-                        <p className="text-lg font-semibold text-[#99571d] md:mt-1">
-                          ₹{order.amount }
-                        </p>
-                      </div>
-                      <div className="lg:col-span-1">
-                        <span
-                          className={` text-center text-xs sm:text-sm font-medium px-3 py-1 rounded-lg ${
-                            order.orderStatus === "delivered"
-                              ? "bg-green-100 text-green-700":
-                                 order.orderStatus === "confirmed"
-                            ? "bg-amber-100 text-amber-600"
-                              : order.orderStatus === "cancelled"
-                              ? "bg-red-100 text-red-600"
-                              : order.orderStatus === "shipped"
-                              ? "bg-blue-100 text-blue-600"
-                              : "bg-yellow-100 text-yellow-600"
-                          }`}
+                    );
+                  })}
+                </div>
+
+                <div className="flex justify-between mt-4">
+                  <p className="text-lg font-bold text-[#99571d]">
+                    Total: ₹{order.amount}
+                  </p>
+
+                  {order.paymentStatus === "paid" ? (
+                    <>
+                      {order.orderStatus === "placed" && (
+                        <button
+                          onClick={() => setCancellingOrder(order)}
+                          className="px-4 py-2 text-sm font-medium border border-[#c74444] text-[#da3838] rounded-md shadow-sm"
                         >
-                          {order.orderStatus === "delivered"
-                            ? "Delivered":
-                          order.orderStatus === "confirmed"
-                            ? "Confirmed"
-                            : order.orderStatus === "cancelled"
-                            ? "Cancelled"
-                            : order.orderStatus === "shipped"
-                            ? "Shipped"
-                            : "Placed"}
-                        </span>
-                      </div>
-                      <div className="lg:col-span-2">
-    {order?.trackingnumber && <div className="flex flex-col items-center gap-0 ">
-                        <span >  {order?.trackingnumber  }</span>
-                        <b>Tracking number</b>
-                          </div>}
-                      </div>
-                      <div className="lg:col-span-1 xl:col-span-2 flex flex-row md:flex-col gap-2 w-full lg:w-fit">
-                        {order.paymentStatus === "paid" ? (<>
-                          {order.orderStatus =="placed" &&
-                          <button
-                            onClick={() => setCancellingOrder(order)}
-                            className="w-full sm:w-auto px-4 py-2 text-sm font-medium border border-[#c74444]  text-[#da3838] rounded-md shadow-sm"
-                          >
-                            Cancel Order
-                          </button>
-                          }
-                          </>
+                          Cancel Order
+                        </button>
+                      )}
+                    </>
+                  ) : (
+                    <button
+                      onClick={() => payNowOrder(order._id, order.amount)}
+                      className="px-4 py-2 text-sm font-medium border border-green-600 text-green-600 rounded-md shadow-sm"
+                    >
+                      Pay Now
+                    </button>
+                  )}
+                </div>
 
-
-                        )
-                      :  <button
-                            onClick={() => payNowOrder(order._id,order.amount)}
-                            className="w-full sm:w-auto px-4 py-2 text-sm font-medium border border-green-600  text-green-600 rounded-md shadow-sm"
-                          >
-                           Pay Now
-                          </button>
-                      }
-                      </div>
-
-                      
-                      </div>
+                {order.trackingnumber && (
+                  <div className="mt-3 text-sm text-gray-700">
+                    Tracking: <b>{order.trackingnumber}</b>
                   </div>
-                );
-              })
-            )}
-          </div>
-        )}
-      </div>
-
-{cancelingOrder && (
-  <div className="h-screen w-screen bg-black/60 fixed top-0 left-0 z-[100] flex items-center justify-center">
-    <div className="bg-white p-6 rounded-xl w-[90%] max-w-md shadow-xl">
-      
-      <h2 className="text-xl font-semibold text-gray-800 mb-3">
-        Cancel Order
-      </h2>
-
-      <p className="text-sm text-gray-600 mb-3">Select a reason:</p>
-
-      <div className="space-y-2">
-        {cancelReasonOptions.map((reason, index) => (
-          <label key={index} className="flex items-center gap-2">
-            <input
-              type="radio"
-              name="cancelReason"
-              value={reason}
-              onChange={(e) => setCancelReason(e.target.value)}
-            />
-            <span>{reason}</span>
-          </label>
-        ))}
-      </div>
-
-      {cancelReason === "Other" && (
-        <textarea
-          className="w-full border border-gray-300 rounded-lg p-2 mt-3"
-          rows="3"
-          placeholder="Enter custom reason"
-          value={customReason}
-          onChange={(e) => setCustomReason(e.target.value)}
-        />
+                )}
+              </div>
+            );
+          })}
+        </div>
       )}
+    </div>
 
-      <div className="flex justify-end gap-3 mt-4">
-        <button
-          onClick={() => setCancellingOrder(false)}
-          className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300"
-        >
-          Close
-        </button>
+    {/* Cancel Popup (Untouched) */}
+    {cancelingOrder && (
+      <div className="h-screen w-screen bg-black/60 fixed top-0 left-0 z-[100] flex items-center justify-center">
+        <div className="bg-white p-6 rounded-xl w-[90%] max-w-md shadow-xl">
+          <h2 className="text-xl font-semibold text-gray-800 mb-3">
+            Cancel Order
+          </h2>
 
-        <button
-          onClick={handleSubmitCancel}
-          className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700"
-        >
-          Submit
-        </button>
+          <p className="text-sm text-gray-600 mb-3">Select a reason:</p>
+
+          <div className="space-y-2">
+            {cancelReasonOptions.map((reason, index) => (
+              <label key={index} className="flex items-center gap-2">
+                <input
+                  type="radio"
+                  name="cancelReason"
+                  value={reason}
+                  onChange={(e) => setCancelReason(e.target.value)}
+                />
+                <span>{reason}</span>
+              </label>
+            ))}
+          </div>
+
+          {cancelReason === "Other" && (
+            <textarea
+              className="w-full border border-gray-300 rounded-lg p-2 mt-3"
+              rows="3"
+              placeholder="Enter custom reason"
+              value={customReason}
+              onChange={(e) => setCustomReason(e.target.value)}
+            />
+          )}
+
+          <div className="flex justify-end gap-3 mt-4">
+            <button
+              onClick={() => setCancellingOrder(false)}
+              className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300"
+            >
+              Close
+            </button>
+
+            <button
+              onClick={handleSubmitCancel}
+              className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
-
-    </div>
+    )}
   </div>
-)}
+);
 
-
-    </div>
-  );
 }
