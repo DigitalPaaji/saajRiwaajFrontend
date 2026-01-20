f09e4fe7fc581d152b0e39130fa53fd899dc1d00
jonty1231 <jontythakur68@gmail.com>
2026-01-20 11:06:21 +0530

    Add

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "https://api.saajriwaaj.com",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
+ images: {
+  remotePatterns: [
+    {
+      protocol: "http",
+      hostname: "api.saajriwaaj.com",
+      pathname: "/uploads/**",
+    },
+  ],
+},
+
 };
 
 export default nextConfig;
